bf9a2646c10883fd755baf5e38e78288cf2bcdaa
Калугин Сергей Викторович <Kalugin@csnp.cea.gov.ru>
2022-08-03 12:03:16 +0300

    Это второй коммит

--- HT7.py
+++ HT7.py
@@ -5,20 +5,25 @@ import os
 Написать программу для подсчета размера всех .py файлов в текущей папке
 '''
 
-# size = {}
-total_size = 0
-path = os.curdir
-for item in os.listdir(path):   
-    if os.path.splitext(item)[-1] == '.py':
-#         size[item] = str(os.path.getsize(os.path.join(os.getcwd(), item))) + ' байт'
-        size = os.path.getsize(os.path.join(os.getcwd(), item))
-        print(f'Размер файла "{item}" составляет {size} байт')
-        total_size += size
-print(f'Общий размер всех файлов с расширением ".py" составляет {total_size} байт')
-# print(size)
+# # size = {}
+# total_size = 0
+# path = os.curdir
+# for item in os.listdir(path):   
+#     if os.path.splitext(item)[-1] == '.py':
+# #         size[item] = str(os.path.getsize(os.path.join(os.getcwd(), item))) + ' байт'
+#         size = os.path.getsize(os.path.join(os.getcwd(), item))
+#         print(f'Размер файла "{item}" составляет {size} байт')
+#         total_size += size
+# print(f'Общий размер всех файлов с расширением ".py" составляет {total_size} байт')
+# # print(size)
   
 '''
 Задача № 2
 Написать программу, которая находит все файла .txt и 
 переносит их в подпапку backup, которую нужно создать.
 '''
+
+## Создание директории 
+os.mkdir('backup') ## Вернет ошибку, если такая папку уже существует
+
+# Необходимо прописать исключения
